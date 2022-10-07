299b529fcad2ae5da6db7e9dd34d3dd9d5aaed9f
HectasYoussef <84864527+HectasYoussef@users.noreply.github.com>
2022-10-07 14:36:10 +0100

    Insert Table invoices & Attachments & Details

--- resources/views/invoices/invoices.blade.php
+++ resources/views/invoices/invoices.blade.php
@@ -24,6 +24,14 @@ List Facture
 				<!-- breadcrumb -->
 @endsection
 @section('content')
+@if (session()->has('Add'))
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+<strong>{{ session()->get('Add') }}</strong>
+<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+<span aria-hidden="true">&times;</span>
+</button>
+</div>
+@endif
 				<!-- row -->
 				<div class="row">
 
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('/', function () {
 });
 
 Auth::routes(["register"=>false]);
+//Auth::routes();
 //Route::get('/{page}', [AdminController::class,"index"]);
 
 
